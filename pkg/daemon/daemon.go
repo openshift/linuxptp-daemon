2acea103293a0b1f72e8a086c1b1c1a08c4ca129
Joseph Richard <joseph.richard@redhat.com>
2021-06-25 00:27:29 -0600

    Allow empty interface string

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -226,9 +226,13 @@ func phc2sysCreateCmd(nodeProfile *ptpv1.PtpProfile) *exec.Cmd {
 
 // ptp4lCreateCmd generate ptp4l command
 func ptp4lCreateCmd(nodeProfile *ptpv1.PtpProfile, confFilePath string) *exec.Cmd {
-	cmdLine := fmt.Sprintf("/usr/sbin/ptp4l -f %s -i %s %s",
+	var ifaceString string;
+	if *nodeProfile.Interface != "" {
+		ifaceString = fmt.Sprintf("-i %s", *nodeProfile.Interface)
+	}
+	cmdLine := fmt.Sprintf("/usr/sbin/ptp4l -f %s %s %s",
 		confFilePath,
-		*nodeProfile.Interface,
+		ifaceString,
 		*nodeProfile.Ptp4lOpts)
 
 	args := strings.Split(cmdLine, " ")
