6c51f170bd29b18379d75ac806cdd9d00122f6f4
Joseph Richard <joseph.richard@redhat.com>
2023-07-31 15:08:34 -0600

    Allow customizing DPLL settings through e810 plugin
    spec:
      profile:
      - name: grandmaster
        plugins:
          e810:
            settings:
              LocalMaxHoldoverOffSet: 1500
              LocalHoldoverTimeout: 14400
              MaxInSpecOffset: 100

--- pkg/dpll/dpll.go
+++ pkg/dpll/dpll.go
@@ -24,6 +24,10 @@ const (
 	LocalHoldoverTimeout   = 14400 //secs
 	MaxInSpecOffset        = 100   //ns
 	monitoringInterval     = 1 * time.Second
+
+	LocalMaxHoldoverOffSetStr = "LocalMaxHoldoverOffSet"
+	LocalHoldoverTimeoutStr   = "LocalHoldoverTimeout"
+	MaxInSpecOffsetStr        = "MaxInSpecOffset"
 )
 
 type DpllConfig struct {
@@ -79,6 +83,7 @@ func (d *DpllConfig) CmdRun(stdToSocket bool) {
 
 func NewDpll(localMaxHoldoverOffSet, localHoldoverTimeout, maxInSpecOffset int64,
 	iface string, dependingState []event.EventSource) *DpllConfig {
+	glog.Infof("Calling NewDpll with localMaxHoldoverOffSet=%d, localHoldoverTimeout=%d, maxInSpecOffset=%d, iface=%s", localMaxHoldoverOffSet, localHoldoverTimeout, maxInSpecOffset, iface)
 	d := &DpllConfig{
 		LocalMaxHoldoverOffSet: localMaxHoldoverOffSet,
 		LocalHoldoverTimeout:   localHoldoverTimeout,
