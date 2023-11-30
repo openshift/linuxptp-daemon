0a9c8dd71121bd105497dc2238c95cd29d0a27f0
Jack Ding <jackding@gmail.com>
2023-11-30 13:31:04 -0500

    Fix event handling
    Fixed the following issues:
    When an event is received with an iface not found in the stored
    in-memory data, the whole Data is re-created before adding DataDetails
    for this event and DataDetails for other ifaces are wiped out. As a
    result, there is only one iface event data is stored in-memory at any
    given time and the GM State calculation is only based on this one iface
    instead of multiple ifaces.
    
    Another issue is the event data for dpll is stored using iface alias,
    while event data for gnss is stored using iface name, resulting same
    interface having multiple entries in in-memory data.
    
    Signed-off-by: Jack Ding <jackding@gmail.com>

--- pkg/event/stats.go
+++ pkg/event/stats.go
@@ -56,3 +56,50 @@ func (d *Data) UpdateState() {
 	d.State = state
 	glog.Infof("state updated for %s =%s", d.ProcessName, d.State)
 }
+
+// GetDataDetails ...
+func (d *Data) GetDataDetails(iface string) *DataDetails {
+	for _, d := range d.Details {
+		if d.IFace == iface {
+			return d
+		}
+	}
+	return nil
+}
+
+func (d *Data) AddEvent(event EventChannel) {
+	for _, dd := range d.Details {
+		if dd.IFace == event.IFace {
+			if dd.time <= event.Time {
+				if dd.State != event.State {
+					if len(StateRegisterer.Subscribers) > 0 {
+						go StateRegisterer.notify(event.ProcessName, event.State)
+					}
+				}
+				dd.State = event.State
+				dd.sourceLost = event.SourceLost
+				dd.ClockType = event.ClockType
+				dd.time = event.Time
+				dd.logData = event.GetLogData()
+			} else {
+				glog.Infof("discarding stale event for process %s, last event @ %d, current event @ %d", event.ProcessName, dd.time, event.Time)
+			}
+			return
+		}
+	}
+
+	details := &DataDetails{
+		ClockType:  event.ClockType,
+		Metrics:    map[ValueType]DataMetric{},
+		IFace:      event.IFace,
+		time:       event.Time,
+		logData:    event.GetLogData(),
+		State:      event.State,
+		sourceLost: event.SourceLost,
+	}
+	d.logData = details.logData
+	d.Details = append(d.Details, details)
+	if len(StateRegisterer.Subscribers) > 0 {
+		go StateRegisterer.notify(event.ProcessName, event.State)
+	}
+}
