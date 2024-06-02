2709cfbe3e9799ecadbb5cb15d8f684d7b752a55
Vitaly Grinberg <v.g@redhat.com>
2024-06-02 04:00:07 -0400

    Add automatic leap file update
    This adds automatic leap file updates from GPS data.
    
    Signed-off-by: Vitaly Grinberg <v.g@redhat.com>

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/openshift/linuxptp-daemon/pkg/config"
 	"github.com/openshift/linuxptp-daemon/pkg/dpll"
+	"github.com/openshift/linuxptp-daemon/pkg/leap"
 
 	"github.com/openshift/linuxptp-daemon/pkg/event"
 	ptpnetwork "github.com/openshift/linuxptp-daemon/pkg/network"
@@ -144,6 +145,8 @@ type Daemon struct {
 
 	// Allow vendors to include plugins
 	pluginManager PluginManager
+
+	leapManager *leap.LeapManager
 }
 
 // New LinuxPTP is called by daemon to generate new linuxptp instance
@@ -159,6 +162,7 @@ func New(
 	refreshNodePtpDevice *bool,
 	closeManager chan bool,
 	pmcPollInterval int,
+	leapManager *leap.LeapManager,
 ) *Daemon {
 	if !stdoutToSocket {
 		RegisterMetrics(nodeName)
@@ -182,7 +186,8 @@ func New(
 			eventChannel:    eventChannel,
 			ptpEventHandler: event.Init(nodeName, stdoutToSocket, eventSocket, eventChannel, closeManager, Offset, ClockState, ClockClassMetrics),
 		},
-		stopCh: stopCh,
+		leapManager: leapManager,
+		stopCh:      stopCh,
 	}
 }
 
@@ -477,6 +482,15 @@ func (dn *Daemon) applyNodePtpProfile(runID int, nodeProfile *ptpv1.PtpProfile) 
 			if e := mkFifo(); e != nil {
 				glog.Errorf("Error creating named pipe, GNSS monitoring will not work as expected %s", e.Error())
 			}
+
+			for _, section := range output.sections {
+				if section.sectionName == "[global]" {
+					section.options["leapfile"] = fmt.Sprintf("%s/%s", config.DefaultLeapConfigPath, os.Getenv("NODE_NAME"))
+					break
+				}
+			}
+			// Write ts2phc.x.conf with leap-file path per node name
+			configOutput, _ = output.renderPtp4lConf()
 			gpsDaemon := &GPSD{
 				name:        GPSD_PROCESSNAME,
 				execMutex:   sync.Mutex{},
@@ -486,6 +500,7 @@ func (dn *Daemon) applyNodePtpProfile(runID int, nodeProfile *ptpv1.PtpProfile) 
 				gmInterface: gmInterface,
 				stopped:     false,
 				messageTag:  messageTag,
+				leapManager: dn.leapManager,
 			}
 			gpsDaemon.CmdInit()
 			gpsDaemon.cmdLine = addScheduling(nodeProfile, gpsDaemon.cmdLine)
