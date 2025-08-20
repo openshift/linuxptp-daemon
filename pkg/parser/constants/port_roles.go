cd51ce14570d71642b1b1a62235a9e2f2ecabc1c
nocturnalastro <micosta@redhat.com>
2025-08-20 11:47:35 +0100

    Add String func to port role constants

--- pkg/parser/constants/port_roles.go
+++ pkg/parser/constants/port_roles.go
@@ -17,3 +17,20 @@ const (
 	// PortRoleListening ...
 	PortRoleListening
 )
+
+func (pr PTPPortRole) String() string {
+	switch pr {
+	case PortRoleSlave:
+		return "SLAVE"
+	case PortRoleMaster:
+		return "MASTER"
+	case PortRolePassive:
+		return "PASSIVE"
+	case PortRoleFaulty:
+		return "FAULTY"
+	case PortRoleListening:
+		return "LISTENING"
+	default:
+		return "UNKNOWN"
+	}
+}
