1b387f72169ce2c4943650cd7e12ebbe30cb6ff5
Vitaly Grinberg <vgrinber@redhat.com>
2025-09-07 11:58:17 +0300

    Send ptp4l event when running without proxy
    Signed-off-by: Vitaly Grinberg <vgrinber@redhat.com>

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -1071,6 +1071,9 @@ func (p *ptpProcess) cmdRun(stdoutToSocket bool, pm *plugin.PluginManager) {
 					printWhenNotEmpty(logfilter.FilterOutput(p.logFilters, output))
 					p.processPTPMetrics(output)
 					if p.name == ptp4lProcessName {
+						if profileClockType == TBC {
+							p.tBCTransitionCheck(output, pm)
+						}
 						if strings.Contains(output, ClockClassChangeIndicator) {
 							go p.updateClockClass(nil)
 						}
