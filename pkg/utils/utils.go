e056ceef80906eb67afade082c41702729deb313
David Elie-Dit-Cosaque <deliedit@redhat.com>
2025-09-10 09:27:20 -0500

    Fix PHC Identifier

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,19 +1,32 @@
 package utils
 
-import "strings"
+import (
+	"regexp"
 
-// GetAlias masks interface names for metric reporting
+	log "github.com/sirupsen/logrus"
+)
+
 func GetAlias(ifname string) string {
 	alias := ""
 	if ifname != "" {
-		// Aliases the interface name or <interface_name>.<vlan>
-		dotIndex := strings.Index(ifname, ".")
-		if dotIndex == -1 {
-			// e.g. ens1f0 -> ens1fx
-			alias = ifname[:len(ifname)-1] + "x"
+		// Single regex to handle both Intel and Mellanox formats with optional VLAN
+		// Intel format: ens1f0, eth0, ens1f0.100 -> ens1fx, ethx, ens1fx.100
+		// Mellanox format: enP2s2f0np0, enP2s2f0np0.100 -> enP2s2fx, enP2s2fx.100
+		pattern := regexp.MustCompile(`^(.+?)(\d+)(?:np\d+)?(\..+)?$`)
+		matches := pattern.FindStringSubmatch(ifname)
+
+		if len(matches) >= 3 {
+			// matches[1] contains the prefix (everything before the last digit sequence)
+			// matches[2] contains the digit sequence to replace
+			// matches[3] contains the VLAN part (including the dot) or empty string
+			alias = matches[1] + "x"
+			if len(matches) > 3 && matches[3] != "" {
+				alias += matches[3] // append VLAN part if present
+			}
 		} else {
-			// e.g ens1f0.100 -> ens1fx.100
-			alias = ifname[:dotIndex-1] + "x" + ifname[dotIndex:]
+			// Interface doesn't match Intel or Mellanox format, return original interface name
+			log.Errorf("Interface %s does not match Intel or Mellanox naming format, using original interface name", ifname)
+			alias = ifname
 		}
 	}
 	return alias
