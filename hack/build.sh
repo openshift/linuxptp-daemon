1fa55617a9526fb4f41d539848f4555f2c418043
Joseph Richard <joseph.richard@redhat.com>
2026-01-28 12:50:19 -0700

    Speedup build

--- hack/build.sh
+++ hack/build.sh
@@ -3,15 +3,6 @@
 ORG_PATH="github.com/k8snetworkplumbingwg"
 REPO_PATH="${ORG_PATH}/linuxptp-daemon"
 
-if [ ! -h .gopath/src/${REPO_PATH} ]; then
-        mkdir -p .gopath/src/${ORG_PATH}
-        ln -s ../../../.. .gopath/src/${REPO_PATH} || exit 255
-fi
-
-export GO15VENDOREXPERIMENT=1
-export GOBIN=${PWD}/bin
-export GOPATH=${PWD}/.gopath
-
 GIT_COMMIT=$(git rev-list -1 HEAD)
 LINKER_RELEASE_FLAGS="-X main.GitCommit=${GIT_COMMIT}"
 go build -ldflags "${LINKER_RELEASE_FLAGS}" --mod=vendor "$@" -o bin/ptp ${REPO_PATH}/cmd
